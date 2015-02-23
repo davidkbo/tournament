a267e3c00446ca75f9714365fb4c07c1a04b1c77
David Kato <davidkbo@gmail.com>
2015-02-23 09:42:30 -0300

    Initial commit with SQL statement ready

---
+++ tournament.sql
@@ -0,0 +1,29 @@
+-- Table definitions for the tournament project.
+-- Put your SQL 'create table' statements in this file; also 'create view'
+-- statements if you choose to use it.
+-- You can write comments in this file by starting them with two dashes, like
+-- these lines here.
+
+CREATE TABLE players (
+	ply_id serial NOT NULL PRIMARY KEY,
+	ply_name varchar(50) NOT NULL  
+);
+
+CREATE TABLE matches (
+	mtc_id serial NOT NULL PRIMARY KEY,
+	ply_id int NOT NULL REFERENCES players(ply_id),
+	match_number int NOT NULL
+);
+
+CREATE TABLE result_types (
+	rst_id serial NOT NULL PRIMARY KEY,
+	result_types varchar(10) NOT NULL
+);
+
+CREATE TABLE match_results (
+	mrs_id serial NOT NULL PRIMARY KEY,
+	ply_id int NOT NULL REFERENCES matches(mtc_id),
+	rst_id int NOT NULL REFERENCES result_types(rst_id)
+);
+
+
