c40b03ed27f25e54c24cd5881701340590f8eaf5
David Kato <davidkbo@gmail.com>
2015-02-27 08:52:35 -0300

    Added comments into tournament.py file and added more details into readme.md

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Udacity (Full Stack Web Developer Nanodegree) - Project P2: Tournament Results
+
+Description:
+Tournament result is a module written in Python that provide functions to deal 
+with Swiss style torunaments with all data stored in a PostgreSQL database.
+
+Functions:
+
+Players
+- registerPlayer(name): Adds a new player into the database
+- deletePlayers(): Deletes all players from the database
+- playerStandings(): Returns the standings from all players
+
+Matches
+- reportMatch(winner, loser): Adds a match result into the database
+- deleteMatches(): Deletes all matches results from the database 
+
+Tournament
+- swissPairings(): Returns the pairs 
+
+Limitations:
+- Just works for even number of player
+- It doesn't support ties
+- It can't deal with multiple tournaments 
+
+Running the program
+- The program requires PostgreSQL 9.3.6 and psycopg2 installed
+- A Vagrant machine with the required can be forked from the github:
+
+git clone http://github.com/udacity/fullstack-nanodegree-vm fullstack
+
+- To use the module you just have to import it (import tournament) and use it
+
+- To run the unit tests to see if everything was set correctly just run:
+
+python tournament_test.py 
+
+- And see if all tests passes
--- tournament.py
+++ tournament.py
@@ -13,9 +13,16 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a DELETE SQL statement to clean the matches table"
     cur.execute("DELETE FROM matches")
+
+    # Commiting all changes and deleting the cursor and closing the database
+    # connection"""
     con.commit()
     cur.close()
     del cur
@@ -23,9 +30,16 @@ def deleteMatches():
 
 def deletePlayers():
     """Remove all the player records from the database."""
+
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a DELETE SQL statement to clean the players table
     cur.execute("DELETE FROM players")
+
+    # Commiting all changes and deleting the cursor and closing the database
+    # connection
     con.commit()
     cur.close()
     del cur
@@ -33,13 +47,24 @@ def deletePlayers():
 
 def countPlayers():
     """Returns the number of players currently registered."""
+
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a SELECT SQL statement to count the number of players and
+    # fetching all results to a variable
+    # The variable is iterated to return the value as a integer
+
     cur.execute("SELECT count(ply_name) FROM players")
     result = cur.fetchall()
+
     outcome = 0
     for row in result:
         outcome = row[0]
+
+    # Commiting all changes and deleting the cursor and closing the database
+    # connection and returning the fuction value
     cur.close()
     del cur
     con.close()
@@ -52,10 +77,15 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a INSERT SQL statement to add new players into the tournament
     cmd = "INSERT INTO players (ply_name) values (%s)"
     cur.execute(cmd, (name,))
+
+    # Commiting all changes and deleting the cursor and closing the database
     con.commit()
     cur.close()
     del cur
@@ -72,14 +102,21 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a SELECT SQL statement to return players standings and
+    # fetching all results to a variable
     cur.execute("select * from players_standings")
     result = cur.fetchall()
-    return result
+
+    # Deleting the cursor, closing the database and returning the function
+    # result
     cur.close()
     del cur
     con.close()
+    return result
 
 def reportMatch(winner, loser):
     """Records the outcome of a single match between two players.
@@ -87,10 +124,15 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a INSERT SQL statement to add matches results
     cmd = "INSERT INTO matches (winner,loser) values (%s,%s)"
     cur.execute(cmd, (winner, loser))
+
+    # Commiting all changes and deleting the cursor and closing the database
     con.commit()
     cur.close()
     del cur
@@ -109,14 +151,23 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    # Connecting to the database and creating a cursor
     con = connect()
     cur = con.cursor()
+
+    # Running a SELECT SQL statement to count the number of players and
+    # fetching all results to a variable, the variable is iterated to
+    # include the pairs tuples into a list
     cur.execute("select * from players_standing_id_name")
     results = cur.fetchall()
+
     pairings = []
     for i in range(0, len(results) - 1, 2):
         pairings.append((results[i] + results[i + 1]))
-    return pairings
+
+    # Deleting the cursor, closing the database and returning the function
+    # result
     cur.close()
     del cur
     con.close()
+    return pairings
